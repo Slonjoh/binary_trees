dc59b6daa4dccca6836617cd6bfa91dff874fa6b
Slonjoh <tobson11122@gmail.com>
2024-01-03 14:55:01 -0800

    right

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -17,16 +17,16 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	new_node = binary_tree_node(parent, value);
 
-    if (new_node == NULL)
-	    return (NULL);
+	if (new_node == NULL)
+		return (NULL);
 
-    if (parent->right != NULL)
-    {
-	    new_node->right = parent->right;
-	    parent->right->parent = new_node;
-    }
+	if (parent->right != NULL)
+	{
+		new_node->right = parent->right;
+		parent->right->parent = new_node;
+	}
 
-    parent->right = new_node;
+	parent->right = new_node;
 
-    return (new_node);
+	return (new_node);
 }
